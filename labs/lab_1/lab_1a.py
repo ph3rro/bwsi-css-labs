a9201559a85b4a64fc2a5ad6619dd253acbbb99d
Aiden <110857052+ph3rro@users.noreply.github.com>
2026-03-21 18:13:08 -0700

    Update lab_1a.py
    print my personal introduction

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Aiden Yun" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("I enjoy coding (robotics, drones, reinforcement learning, other ML stuff) and gaming (THE FINALS, Arc Raiders, Rocket League, Minecraft), hmu. A fun fact about me is that I spend a lot of my time hitting a rubber ball around a wooden box—also known as squash! Excited to be here!")
 
 if __name__ == "__main__":
     main()
